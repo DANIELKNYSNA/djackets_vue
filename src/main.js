0bafa2776f93878066b05d1cef82cd91e6e5dfb4
Daniel Murphy <knysnacoder@gmail.com>
2022-09-07 11:09:18 +0200

    Init

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
-createApp(App).use(store).use(router).mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+
+createApp(App).use(store).use(router, axios).mount('#app')
